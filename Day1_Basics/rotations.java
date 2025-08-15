c57c91b1741eb9c838db0bf429a759d0cbb212c7
Navaneeth veer <navaneeth42.com@gmail.com>
2025-08-15 14:41:12 +0530

    Create rotations.java

---
+++ Day1_Basics/rotations.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class rotation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String temp = str;
+		int k = sc.nextInt();
+		sc.close();
+		int n = str.length();
+		k = k%n;
+		char[] arr = str.toCharArray();
+		
+		//Left Rotation;
+		reverse(arr,0,n-k-1);
+		reverse(arr,n-k,n-1);
+		reverse(arr,0,n-1);
+		str = new String(arr);
+		System.out.println("Left Rotated String is : "+str);
+		str = temp;
+		arr = str.toCharArray();
+		
+		//Right Rotation;
+		reverse(arr,0,k-1);
+		reverse(arr,k,n-1);
+		reverse(arr,0,n-1);
+		str = new String(arr);
+		System.out.println("Right Rotated String is : "+str);
+	}
+	static void reverse(char[] arr, int i , int j) {
+		while(i<=j) {
+			char c = arr[i];
+			arr[i] = arr[j];
+			arr[j] = c;
+			i++;
+			j--;
+		}
+	}
+
+}
