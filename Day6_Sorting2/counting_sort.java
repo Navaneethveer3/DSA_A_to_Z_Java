a24f7a42787f284553dd2d203393913e2922557f
Navaneeth veer <navaneeth42.com@gmail.com>
2025-08-20 14:43:49 +0530

    Update counting_sort.java

--- Day6_Sorting2/counting_sort.java
+++ Day6_Sorting2/counting_sort.java
@@ -1,20 +1,22 @@
 public class counting_sort {
 
 	public static void main(String[] args) {
+		
+		// Tc : O(n+k) , where k is the range of the array;
 		int[] nums = {4,1,0,1,2,3,4,4,1,2};
 		int len = 0;
 		for(int i : nums) {
 			if(len<i) len = i;
 		}
-    
-    // Count frequency of all the values;
+		
+		// Count frequency of all the values;
 		int[] count = new int[len+1];
 		for(int i : nums) {
 			count[i] += 1;
 		}
 		int ind = 0;
-    
-    // Copy the values to the array until the counter becomes 0 for the particular values;
+		
+		// Copy the values to the array until the counter becomes 0 for the particular values;
 		for(int i=0;i<count.length;i++) {
 			while(count[i]!=0) {
 				nums[ind++] = i;
