80bd0437b4cf8caff42503633002ba8726ab8abc
Navaneeth veer <navaneeth42.com@gmail.com>
2025-09-08 09:27:31 +0530

    Create balanced_binary_tree.java

---
+++ Day16_Trees/balanced_binary_tree.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(height(root)==-1) return false;
+        return true;
+    }
+    int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left = height(root.left);
+        if(left==-1){
+            return -1;
+        }
+        int right = height(root.right);
+        if(right==-1){
+            return -1;
+        }
+        if(Math.abs(left-right)>1){
+            return -1;
+        }
+        return 1+Math.max(left,right);
+    }
+}
